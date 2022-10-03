6b77e0bd0164b0bdc85a4556d9996ad591ee6b3a
Umirov Jobir <jobirumirovmoliya@gmail.com>
2022-10-03 11:02:58 +0500

    comment APIs

--- services/admin.py
+++ services/admin.py
@@ -1,41 +1,41 @@
-from ast import Or
-from pyexpat import model
-from django.contrib import admin
-from services.models.system_info import SystemInfoModel, SystemInfoFileModel
-from services.models.service import ServiceModel, Category, MenuModel
-from services.models.restoran import TableModel, RestoranModel, BookedDate, EvantModel
-from services.models.order import Order
-from django.contrib.auth.models import Group
-admin.site.unregister(Group)
+# from ast import Or
+# from pyexpat import model
+# from django.contrib import admin
+# from services.models.system_info import SystemInfoModel, SystemInfoFileModel
+# from services.models.service import ServiceModel, Category, MenuModel
+# from services.models.restoran import TableModel, RestoranModel, BookedDate, EvantModel
+# from services.models.order import Order
+# from django.contrib.auth.models import Group
+# admin.site.unregister(Group)
 
-import nested_admin
+# import nested_admin
 
-@admin.action(description='Mark selected stories as published')
-def make_published(modeladmin, request, queryset):
-    queryset.update(status='Canceled')
+# @admin.action(description='Mark selected stories as published')
+# def make_published(modeladmin, request, queryset):
+#     queryset.update(status='Canceled')
 
-class OrderAdmin(admin.ModelAdmin):
-    # list_display = ['user_id', 'day']
-    actions = [make_published]
-admin.site.register(Order, OrderAdmin)
+# class OrderAdmin(admin.ModelAdmin):
+#     # list_display = ['user_id', 'day']
+#     actions = [make_published]
+# admin.site.register(Order, OrderAdmin)
 
-admin.site.register(SystemInfoModel)
+# admin.site.register(SystemInfoModel)
 
-admin.site.register(SystemInfoFileModel)
+# admin.site.register(SystemInfoFileModel)
 
 
-admin.site.register(EvantModel)
+# admin.site.register(EvantModel)
 
-admin.site.register(RestoranModel)
-admin.site.register(TableModel)
-admin.site.register(BookedDate)
+# admin.site.register(RestoranModel)
+# admin.site.register(TableModel)
+# admin.site.register(BookedDate)
 
-admin.site.register(MenuModel)
+# admin.site.register(MenuModel)
 
-class ServicesInline(nested_admin.NestedStackedInline):
-    model = ServiceModel
-class CategoryAdmin(nested_admin.NestedModelAdmin):
-    inlines = [ServicesInline,]
-admin.site.register(Category, CategoryAdmin)
+# class ServicesInline(nested_admin.NestedStackedInline):
+#     model = ServiceModel
+# class CategoryAdmin(nested_admin.NestedModelAdmin):
+#     inlines = [ServicesInline,]
+# admin.site.register(Category, CategoryAdmin)
 
-# admin.site.register(Order)
+# # admin.site.register(Order)
--- services/urls.py
+++ services/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from services.views import SystemView, EvantView, ServiceView, TableView, RestoranView, MenuView, OrderView, SystemFileView
 
 
-urlpatterns = [
-    path('info/', SystemView.as_view()),
-    path('gallery/', SystemFileView.as_view()),
-    path('events/', EvantView.as_view()),
-    path('restorans/', RestoranView.as_view()),
-    path('menus/', MenuView.as_view()),
-    path('tables/', TableView.as_view()),
-    path('order/', OrderView.as_view()),
-    path('services/', ServiceView.as_view()),
-]
+# urlpatterns = [
+#     path('info/', SystemView.as_view()),
+#     path('gallery/', SystemFileView.as_view()),
+#     path('events/', EvantView.as_view()),
+#     path('restorans/', RestoranView.as_view()),
+#     path('menus/', MenuView.as_view()),
+#     path('tables/', TableView.as_view()),
+#     path('order/', OrderView.as_view()),
+#     path('services/', ServiceView.as_view()),
+# ]
